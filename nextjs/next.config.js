3fb5df9ef976d83a6e24141fc1102ef56b9100dc
Mario Torres <mariotee10@gmail.com>
2019-08-13 14:42:36 -0500

    added a hack for process.env to static

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
 const webpack = require('webpack')
 const withCss = require('@zeit/next-css')
+const path = require('path')
 
 module.exports = withCss(
     {
         webpack: (config) => {
             config.plugins.push(new webpack.EnvironmentPlugin(process.env))
+            config.plugins.push(new webpack.DefinePlugin({
+                //this is kind of a hack, but i guess it beats using Linux sed
+                'process.env.SERVER_HOST': JSON.stringify(process.env.SERVER_HOST)
+            }))
+            config.resolve.modules.push(path.resolve('./'))
 
             return config
         },
